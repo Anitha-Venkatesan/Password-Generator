5da14d579c4458a079fc48699f8338ab074bcee7
Anitha Venkatesan <anithamca68@gmail.com>
2020-04-15 20:40:26 -0500

    changed var names and removed log

--- script.js
+++ script.js
@@ -16,106 +16,86 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 var lowerLetters ="abcdefghijklmnopqrstuvwxyz";
-var lowerArr = lowerLetters.split('');
-
 var upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var upperArr = upperLetters.split('');
-
 var numbers ="1234567890";
-var numberArr = numbers.split('');
-
-
 var symbols ="!@#$%^&*()?/\\~{}'`\"";
+
+var lowerArr = lowerLetters.split('');
+var upperArr = upperLetters.split('');
+var numberArr = numbers.split('');
 var symbolArr= symbols.split('');
 
 
 
 function generatePassword() {
-  alert ("Hello World!Type the secure password");
-  var passLength = prompt("Enter the password length");
-    if(passLength <8 || passLength > 128)
-    {
-      alert("Password Length must be 8-128 characters");
-    }
+  
+    alert ("Hello World!Type the secure password");
+    var passLength = prompt("Enter the password length");
     passLength = parseInt(passLength);
     
+      if(passLength <8 || passLength > 128){
+        alert("Password Length must be 8-128 characters");
+      }
     
-    var inputLower = confirm("Do you want to use Lowercase?"); 
-    var inputUpper = confirm("Do you want to use Uppercase?"); 
-    var inputNumber = confirm("Do you want to use Numbers?"); 
-    var inputSymbol = confirm("Do yo want to use Symbols?");
+      var inputLower = confirm("Do you want to use Lowercase?"); 
+      var inputUpper = confirm("Do you want to use Uppercase?"); 
+      var inputNumber = confirm("Do you want to use Numbers?"); 
+      var inputSymbol = confirm("Do yo want to use Symbols?");
 
-  
-    if(inputLower || inputUpper || inputNumber || inputSymbol)
-    {
-      
-      
-      var index;
-      var confirmPassword = '';
-      var newString= [];
+      if(inputLower || inputUpper || inputNumber || inputSymbol)
+      {
+        var index;
+        var confirmPassword = '';
+        var charTypes= [];
           
         if (inputLower) {
 
-
           index = Math.floor(Math.random() * lowerArr.length);
           confirmPassword += lowerArr[index];
-          console.log("inputLower password:" +confirmPassword);
-          newString = newString.concat(lowerArr);
-          
+          charTypes = charTypes.concat(lowerArr);
          
         }
 
         if (inputUpper) {
-          
-         
+
           index = Math.floor(Math.random() * upperArr.length);
           confirmPassword += upperArr[index];
-          console.log("inputUpper password:" +confirmPassword);
-          newString = newString.concat(upperArr);
-          
+          charTypes = charTypes.concat(upperArr);
 
         }
 
         if (inputNumber) {
 
-          
           index = Math.floor(Math.random() * numberArr.length);
           confirmPassword += numberArr[index];
-          console.log("inputNumber password:" +confirmPassword);
-          newString = newString.concat(numberArr);
-          
-          
+          charTypes = charTypes.concat(numberArr);
+    
         }
 
         if (inputSymbol) {
-          
-         
+           
           index = Math.floor(Math.random() * symbolArr.length);
           confirmPassword += symbolArr[index];
-          console.log("inputSymbol password:" +confirmPassword);
-          newString = newString.concat(symbolArr);
-          
-
+          charTypes = charTypes.concat(symbolArr);
         
         }
         
         passLength = passLength - confirmPassword.length;
 
         for (i=0; i< passLength; i++){
-          index = Math.floor(Math.random() * newString.length);
-          console.log(newString);
-          confirmPassword += newString[index];
+        
+          index = Math.floor(Math.random() * charTypes.length);
+          confirmPassword += charTypes[index];
           console.log("Confirm password:" +confirmPassword);
         }
 
-        return confirmPassword;
-      
-    }
-    else{  
+        return confirmPassword;  
+      }
+     
+       else{  
         alert("Select atleast one option above");   
       }         
-    
-    
+
 }
 
 
