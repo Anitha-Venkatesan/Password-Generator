ab43e1d6398b512ef421a797b66b451f235f4ff1
Anitha Venkatesan <anithamca68@gmail.com>
2020-04-15 21:19:25 -0500

    Added comment

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function writePassword() {
   var password = generatePassword();
 
   var passwordText = document.querySelector("#password");
-     
+  
   passwordText.value = password;
 
 }
@@ -15,10 +15,12 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+
 var lowerLetters ="abcdefghijklmnopqrstuvwxyz";
 var upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numbers ="1234567890";
 var symbols ="!@#$%^&*()?/\\~{}'`\"";
+
 
 var lowerArr = lowerLetters.split('');
 var upperArr = upperLetters.split('');
@@ -31,17 +33,22 @@ function generatePassword() {
   
     alert ("Hello World!Type the secure password");
     var passLength = prompt("Enter the password length");
+
+    //--converting the password length from string to integer--
     passLength = parseInt(passLength);
     
       if(passLength <8 || passLength > 128){
         alert("Password Length must be 8-128 characters");
+        
       }
     
+      
       var inputLower = confirm("Do you want to use Lowercase?"); 
       var inputUpper = confirm("Do you want to use Uppercase?"); 
       var inputNumber = confirm("Do you want to use Numbers?"); 
       var inputSymbol = confirm("Do yo want to use Symbols?");
 
+      //--Atleast one character type should be selected--
       if(inputLower || inputUpper || inputNumber || inputSymbol)
       {
         var index;
@@ -49,7 +56,7 @@ function generatePassword() {
         var charTypes= [];
           
         if (inputLower) {
-
+          //--Randomly select a lower case character-- 
           index = Math.floor(Math.random() * lowerArr.length);
           confirmPassword += lowerArr[index];
           charTypes = charTypes.concat(lowerArr);
@@ -57,7 +64,7 @@ function generatePassword() {
         }
 
         if (inputUpper) {
-
+          //--Randomly select a upper case character-- 
           index = Math.floor(Math.random() * upperArr.length);
           confirmPassword += upperArr[index];
           charTypes = charTypes.concat(upperArr);
@@ -65,7 +72,7 @@ function generatePassword() {
         }
 
         if (inputNumber) {
-
+          //--Randomly select a number--       
           index = Math.floor(Math.random() * numberArr.length);
           confirmPassword += numberArr[index];
           charTypes = charTypes.concat(numberArr);
@@ -73,13 +80,14 @@ function generatePassword() {
         }
 
         if (inputSymbol) {
-           
+           //--Randomly select a symbol-- 
           index = Math.floor(Math.random() * symbolArr.length);
           confirmPassword += symbolArr[index];
           charTypes = charTypes.concat(symbolArr);
         
         }
-        
+        //--randomly selected a character from each types ,the below will randomly generate the password 
+        //--to match rest of password length entered
         passLength = passLength - confirmPassword.length;
 
         for (i=0; i< passLength; i++){
@@ -92,7 +100,7 @@ function generatePassword() {
         return confirmPassword;  
       }
      
-       else{  
+      else{  
         alert("Select atleast one option above");   
       }         
 
